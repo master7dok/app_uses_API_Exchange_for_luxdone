fda5d8567466a9d071061537109357b49fbc9dd0
Master-DOK <master7dok@gmail.com>
2021-03-27 16:23:00 +0100

    new commit

--- app.js
+++ app.js
@@ -1,15 +1,33 @@
-
-const path = require('path')
 const express = require('express')
-const PORT = process.env.PORT ?? 3000
+const path = require('path')
 const app = express()
+const PORT = process.env.PORT || 3000
+const url = 'https://api.exchangeratesapi.io/latest'
 
-app.use(express.static(path.resolve(__dirname, 'client')))
+const https = require('https');
+let textJSON
 
-app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'index.html'))
+app.use(express.static('client'))
+
+https.get(url,(res) => {
+    let body = "";
+
+    res.on("data", (chunk) => {
+        body += chunk;
+        textJSON=body;
+    });
+
+}).on("error", (error) => {
+    console.error(error.message);
+})
+app.get('/', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '/client/index.html'))
+
+})
+app.get('/api/getex', (req, res) => {
+    res.send(textJSON)
 })
 
 app.listen(PORT, () => {
-    console.log(`Server has been running on port ${PORT}...`)
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
---
+++ client/main.js
@@ -0,0 +1,142 @@
+// Global Variables
+
+const addCurrencyList = document.querySelector(".add-currency-list");
+const currenciesList = document.querySelector(".currencies");
+
+const dataURL = "http://localhost:3000/api/getex";
+
+const initiallyDisplayedCurrencies = ["USD", "EUR", "PLN", "RUB"];
+let baseCurrency;
+let baseCurrencyAmount;
+
+let currencies = [
+  {
+    name: "US Dollar",
+    abbreviation: "USD",
+    symbol: "\u0024",
+    flagURL: "img/usd.jpg"
+  },
+  {
+    name: "Polish Zloty",
+    abbreviation: "PLN",
+    symbol: "\u007A\u0142",
+    flagURL: "img/pl.png"
+  },
+  {
+    name: "Euro",
+    abbreviation: "EUR",
+    symbol: "\u20AC",
+    flagURL: "img/eu.png"
+  },
+  {
+    name: "Russian Ruble",
+    abbreviation: "RUB",
+    symbol: "\u20BD",
+    flagURL: "img/ru.png"
+  }
+
+];
+
+// Event Listeners
+
+function setNewBaseCurrency(newBaseCurrencyLI) {
+  newBaseCurrencyLI.classList.add("base-currency");
+  baseCurrency = newBaseCurrencyLI.id;
+  const baseCurrencyRate = currencies.find(currency => currency.abbreviation===baseCurrency).rate;
+  currenciesList.querySelectorAll(".currency").forEach(currencyLI => {
+    const currencyRate = currencies.find(currency => currency.abbreviation===currencyLI.id).rate;
+    const exchangeRate = currencyLI.id===baseCurrency ? 1 : (currencyRate/baseCurrencyRate).toFixed(4);
+    currencyLI.querySelector(".base-currency-rate").textContent = `1 ${baseCurrency} = ${exchangeRate} ${currencyLI.id}`;
+  });
+}
+
+currenciesList.addEventListener("input", currenciesListInputChange);
+
+function currenciesListInputChange(event) {
+  const isNewBaseCurrency = event.target.closest("li").id!==baseCurrency;
+  if(isNewBaseCurrency) {
+    currenciesList.querySelector(`#${baseCurrency}`).classList.remove("base-currency");
+    setNewBaseCurrency(event.target.closest("li"));
+  }
+  const newBaseCurrencyAmount = isNaN(event.target.value) ? 0 : Number(event.target.value);
+  if(baseCurrencyAmount!==newBaseCurrencyAmount || isNewBaseCurrency) {
+    baseCurrencyAmount = newBaseCurrencyAmount;
+    const baseCurrencyRate = currencies.find(currency => currency.abbreviation===baseCurrency).rate;
+    currenciesList.querySelectorAll(".currency").forEach(currencyLI => {
+      if(currencyLI.id!==baseCurrency) {
+        const currencyRate = currencies.find(currency => currency.abbreviation===currencyLI.id).rate;
+        const exchangeRate = currencyLI.id===baseCurrency ? 1 : (currencyRate/baseCurrencyRate).toFixed(4);
+        currencyLI.querySelector(".input input").value = exchangeRate*baseCurrencyAmount!==0 ? (exchangeRate*baseCurrencyAmount).toFixed(4) : "";
+      }
+    });
+  }
+}
+
+currenciesList.addEventListener("focusout", currenciesListFocusOut);
+
+function currenciesListFocusOut(event) {
+  const inputValue = event.target.value;
+  if(isNaN(inputValue) || Number(inputValue)===0) event.target.value="";
+  else event.target.value = Number(inputValue).toFixed(4);
+}
+
+currenciesList.addEventListener("keydown", currenciesListKeyDown);
+
+function currenciesListKeyDown(event) {
+  if(event.key==="Enter") event.target.blur();
+}
+
+// Auxiliary Functions
+
+function populateAddCyrrencyList() {
+  for(let i=0; i<currencies.length; i++) {
+    addCurrencyList.insertAdjacentHTML(
+        "beforeend",
+        `<li data-currency=${currencies[i].abbreviation}>
+        <img src=${currencies[i].flagURL} class="flag"><span>${currencies[i].abbreviation} - ${currencies[i].name}</span>
+      </li>`
+    );
+  }
+}
+
+function populateCurrenciesList() {
+  for(let i=0; i<initiallyDisplayedCurrencies.length; i++) {
+    const currency = currencies.find(c => c.abbreviation===initiallyDisplayedCurrencies[i]);
+    if(currency) newCurrenciesListItem(currency);
+  }
+}
+
+function newCurrenciesListItem(currency) {
+  if(currenciesList.childElementCount===0) {
+    baseCurrency = currency.abbreviation;
+    baseCurrencyAmount = 0;
+  }
+  addCurrencyList.querySelector(`[data-currency=${currency.abbreviation}]`).classList.add("disabled");
+  const baseCurrencyRate = currencies.find(c => c.abbreviation===baseCurrency).rate;
+  const exchangeRate = currency.abbreviation===baseCurrency ? 1 : (currency.rate/baseCurrencyRate).toFixed(4);
+  const inputValue = baseCurrencyAmount ? (baseCurrencyAmount*exchangeRate).toFixed(4) : "";
+
+  currenciesList.insertAdjacentHTML(
+      "beforeend",
+      `<li class="currency ${currency.abbreviation===baseCurrency ? "base-currency" : ""}" id=${currency.abbreviation}>
+      <img src=${currency.flagURL} class="flag">
+      <div class="info">
+        <p class="input"><span class="currency-symbol">${currency.symbol}</span><input placeholder="0.0000" value=${inputValue}></p>
+        <p class="currency-name">${currency.abbreviation} - ${currency.name}</p>
+        <p class="base-currency-rate">1 ${baseCurrency} = ${exchangeRate} ${currency.abbreviation}</p>
+      </div>
+      <span class="close">&times;</span>
+    </li>`
+  );
+}
+
+fetch(dataURL)
+    .then(res => res.json())
+    .then(data => {
+      document.querySelector(".date").textContent = data.date;
+      data.rates["EUR"] = 1;
+      currencies.forEach(currency => currency.rate = data.rates[currency.abbreviation]);
+      populateAddCyrrencyList();
+      populateCurrenciesList();
+    })
+    .catch(err => console.log(err));
